2139cbd6627d9aa01b5d883309136dbf47df97b6
linyuanteng <daniellam.yt@gmail.com>
2022-09-09 14:43:23 +0800

    feat: 新增资源目录及自动导入

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+// 配置信息
+const config = require("./src/config");
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        // 自动导入全局 scss 文件，动态导入主题 scss 文件
+        prependData: `
+          @import "~@/assets/styles/index.scss";
+          @import "~@/assets/themes/${config.theme}/styles/index.scss";
+        `,
+      },
+    },
+  },
+};
