9122258752a12f2ff1e24499db7286689ac3efc7
linyuanteng <daniellam.yt@gmail.com>
2022-09-13 14:41:46 +0800

    feat: 状态管理新增自动导入及持久化功能

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import { camelCase } from "lodash";
+import { LocalStorage } from "@/utils/storage";
 
 Vue.use(Vuex);
 
+// 自动导入 modules 目录下所有模块的内容
+const context = require.context("./modules", true, /\.store$/);
+const modules = context.keys().reduce((result, key) => {
+  /*
+    模块名使用驼峰命名法，如：
+    xxx.store.js 对应的模块名为 xxx
+    xxx-yyy.store.js 对应的模块名为 xxxYyy
+   */
+  const moduleName = camelCase(key).replace("Store", "");
+  const moduleContent = context(key);
+  result[moduleName] = moduleContent.default || moduleContent;
+  return result;
+}, {});
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: modules,
+  plugins: [
+    // 状态持久化插件
+    createPersistedState({
+      storage: new LocalStorage(),
+    }),
+  ],
 });
